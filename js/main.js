5489d80ed764aec0e5e248a5d7b01fcc8e3b977e
nadamedhat726 <nadamedhat726@gmail.com>
2025-09-21 02:59:22 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ searchBtn.onclick = () => {
 
 async function getWeather(city) {
   try {
-    let res = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=c49b0f7ea9d049c9ac4224238251809&q=${city}&days=3`);
+    let res = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=c49b0f7ea9d049c9ac4224238251809&q=${city}&days=3`);
     let data = await res.json();
     showWeather(data);
     localStorage.setItem("lastWeather", JSON.stringify(data));
